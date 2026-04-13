46efae01d241537dfbb46952cc146941c3c8466d
root <root@localhost.localdomain>
2026-04-13 16:02:57 +0000

    fix: add GeneratedPluginRegistrant

---
+++ android/app/src/main/kotlin/com/example/audio_player/GeneratedPluginRegistrant.kt
@@ -0,0 +1,11 @@
+package com.example.audio_player
+
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class GeneratedPluginRegistrant {
+    companion object {
+        fun registerWith(registry: io.flutter.plugin.common.PluginRegistry) {
+            GeneratedPluginRegistrant.registerWith(registry)
+        }
+    }
+}
--- android/app/src/main/kotlin/com/example/audio_player/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/audio_player/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.audio_player
 
 import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
 
-class MainActivity: FlutterFragmentActivity()
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        io.flutter.plugins.GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
+}
